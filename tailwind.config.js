08b298fabf27d445a3639e31f4446b088e561754
stmeem <stmeem@yahoo.com>
2021-11-06 13:34:31 +0600

    customized colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-   
+    extend:{
+    colors: {
+      black: {
+        light: '#0c0c0c',
+        default: '#000000',
+        
+      }
+    }
+    }
   },
   variants: {
     extend: {},
